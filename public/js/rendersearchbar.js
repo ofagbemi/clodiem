3cf5ce1dc87155c7f332636c7a9985e01b02ded7
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-05 21:56:04 -0800

    Fixed search text box display problems

--- public/js/rendersearchbar.js
+++ public/js/rendersearchbar.js
@@ -9,13 +9,18 @@ function rendersearchbar(h, barheight) {
   $('body').append('<div class="searchwrap"><div class="search"></div></div>');
   $('.searchwrap')
     .css('top', winheight * h + 'px')
-    .css('height', height + 'px')
-    .css('width', winwidth + 'px');
+    //.css('height', height + 'px')
+    .css('width', winwidth + 'px')
+    .css('overflow', 'hidden');
 
   var label = '<label style="height: ' + (height - 8) + 'px; width: ' + (height) + 'px;" id="searchbox_label" for="searchbox"></label>';
   var input = '<input style="height: ' + (height - 8) + 'px;" id="searchbox" placeholder="Search">';
   $('.search')
     .css('padding', '4px ' + winwidth * 0.1 + 'px')
     .append(label)
-    .append(input);
+    .append(input)
+    .append('<div class="clear"></div>');
+    
+  $('#searchbox')
+    .css('width', ($('.search').width() - $('#searchbox_label').width()) + 'px');
 }
