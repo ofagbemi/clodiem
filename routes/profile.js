19b1bdb4e3e87c340e245abf150d85b138e4772e
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-10 14:41:05 -0800

    Added styles posts to closet. Do not yet display when the user clicks on a post.

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,17 @@ exports.view = function(req, res) {
   for(var i=0;i<u['post_ids'].length;i++) {
     u['posts'].push(data['posts'][u['post_ids'][i]]);
   }
+  u['styles'] = [];
+  for(var i=0;i<u['style_ids'].length;i++) {
+    var styles = data['posts'][u['style_ids'][i]];
+    styles['posts'] = [];
+    for(var j=0;j<styles['items'].length;j++) {
+      styles['posts'].push(data['posts'][u['post_ids'][j]]);
+    }
+    
+    u['styles'].push(styles);
+    
+  }
   
   res.render('profile', u);
 };
