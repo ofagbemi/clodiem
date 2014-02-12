c8c0d0f8a1f0c4c40091ab61ce2cb86941993f49
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-12 12:16:06 -0800

    If a user tries to go to the aisle page without logging in or signing up, they'll now be redirected to the log in page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,25 +1,31 @@
 var data = require("../data.json");
 
 exports.view = function(req, res) {
-  var ret = {};
-  ret['logged_in_username'] = data['logged_in_username'];
-  ret['logged_in_user_img'] = data['logged_in_user_img'];
-  ret['posts'] = [];
-  for(var i=0;i<data['aisle_post_ids'].length;i++) {
-    var post = data['posts'][data['aisle_post_ids'][i]];
-    ret['posts'].push(post);
-  }
+  if(data['logged_in_user']) {
+	var ret = {};
+	ret['logged_in_username'] = data['logged_in_username'];
+	ret['logged_in_user_img'] = data['logged_in_user_img'];
+	ret['posts'] = [];
+	for(var i=0;i<data['aisle_post_ids'].length;i++) {
+	  var post = data['posts'][data['aisle_post_ids'][i]];
+	  ret['posts'].push(post);
+	}
   
-  for(var i=0;i<ret['posts'].length;i++) {
-	var post = ret['posts'][i];
-	post['items'] = [];
-	if(post['item_ids']) {
-	  // turn items (item/post ids) into their actual posts
-	  for(var j=0;j<post['item_ids'].length;j++) {
-	    var item = data['posts'][post['item_ids'][j]];
-	    post['items'].push(item);
+	for(var i=0;i<ret['posts'].length;i++) {
+	  var post = ret['posts'][i];
+	  post['items'] = [];
+	  if(post['item_ids']) {
+		// turn items (item/post ids) into their actual posts
+		for(var j=0;j<post['item_ids'].length;j++) {
+		  var item = data['posts'][post['item_ids'][j]];
+		  post['items'].push(item);
+		}
 	  }
 	}
+	
+	res.render('dashboard', ret);
+  } else {
+    // if no one's logged in, redirect to log in page
+    res.redirect('/login');
   }
-  res.render('dashboard', ret);
 };
