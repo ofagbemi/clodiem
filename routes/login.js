8fab46db6f7901c59a99be742d7be1b577f9c700
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-12 13:34:31 -0800

    Fixed bug in login.js that prevented users from actually logging in

--- routes/login.js
+++ routes/login.js
@@ -5,14 +5,19 @@ exports.view = function(req, res) {
   res.render('login', {});
 };
 
-exports.setcurrentuser = function(userid) {
+function setcurrentuser(userid) {
   console.log('login.js: setting current user to ' + userid);
   data['logged_in_user'] = data['users'][userid];
 }
 
+exports.setcurrentuser = setcurrentuser;
+
 exports.loginuser = function(req, res) {
   // look for user -- TODO: change this later to generate user id
   var userid = util.getuserid(req.query.username);
-  setcurrentuser(userid);
-  res.redirect('/aisle');
+  if(data['users'][userid]) {
+    setcurrentuser(userid);
+    res.redirect('/aisle');
+  }
+  console.log('login.js: login failed! User ' + userid + ' could not be found');
 };
