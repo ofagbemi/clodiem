47ec85acb32c5f357593ef6e30091e65dac28599
Jean Kono <jkono@stanford.edu>
2014-02-12 14:47:16 -0800

    search

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,25 @@
+var data = require("../data.json");
+
 exports.view = function(req, res) {
-  res.render('search', {'query': req.query.q});
+    
+    var d = data['posts'];
+    var ids = data['aisle_post_ids'];
+    
+    var posts = [];
+    posts['query'] = req.query.q;
+    posts['posts'] = [];
+    
+    for(var i=0; i < ids.length; i++) {
+        if(d[ids[i]]['tags']) {
+            for(var j=0; j< d[ids[i]]['tags'].length; j++){
+                if(d[ids[i]]['tags'][j]['tag'] === req.query.q){
+                    posts['posts'].push(d[ids[i]]);
+                }
+            }
+        }
+    }
+    
+    
+    
+    res.render('search', posts);
 };
