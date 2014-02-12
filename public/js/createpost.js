4b258ad3b2b81cf8ba17b601986fe133c9073d6f
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-11 22:48:08 -0800

    Removed stay alert from createpost.js

--- public/js/createpost.js
+++ public/js/createpost.js
@@ -6,10 +6,7 @@ function createpost_bindclicklisteners() {
   $('a.img_upload')
     .unbind('click')
     .click(function() {
-      alert('hello');
       $('input.img_upload').trigger('click');
-      
-      
       $('a.img_upload').html($('a.img_upload').html().replace('Upload a photo', 'Upload a different photo'));
     });
 }
