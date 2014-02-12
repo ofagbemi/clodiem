c7cda6740bd3c4f6d5fd1e03c8e9dc11bbb40703
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-11 22:02:48 -0800

    Got like buttons functioning properly (demo level)

--- public/js/likebuttons.js
+++ public/js/likebuttons.js
@@ -1,6 +1,18 @@
 function activatelikebuttons() {
   $('.like_button').click(function(e) {
     e.preventDefault();
-    alert('Thanks!');
+    var num_likes_div = $(this).parent().parent().find('.num_likes');
+    var num_likes = parseInt(num_likes_div.html());
+    if($(this).attr('status') == 'liked') {
+      num_likes--;
+      $(this).attr('status', '');
+      $(this).css('background-image', "url('../images/icons/black_heart/black_heart.svg')");
+    } else {
+      num_likes++;
+      $(this).attr('status', 'liked');
+      $(this).css('background-image', "url('../images/icons/black_heart/red_heart.svg')");
+    }
+    
+    num_likes_div.html(num_likes);
   });
 }
