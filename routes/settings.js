04615abdb9a0db21a58ac94cac2281be360136c5
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-13 18:35:57 -0800

    Trying to access the settings page without logging in kicks user to login page

--- routes/settings.js
+++ routes/settings.js
@@ -1,3 +1,11 @@
+var data = require('../data.json');
+
 exports.view = function(req, res) {
-  res.render('settings', {});
+  var ret = {};
+  if(data['logged_in_user']) {
+    ret['logged_in_user'] = data['logged_in_user'];
+    res.render('settings', ret);
+  } else {
+    res.redirect('/login');
+  }
 }
