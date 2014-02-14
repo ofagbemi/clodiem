0e44bd8cd53828726b496dcba46f85bde3e518bc
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-14 11:53:06 -0800

    Set search to look at all posts instead of aisle posts

--- routes/search.js
+++ routes/search.js
@@ -1,25 +1,26 @@
 var data = require("../data.json");
 
 exports.view = function(req, res) {
+    var ret = {};
+    
+    // get the current logged in user
+    ret['logged_in_user'] = data['logged_in_user'];
+    
+    ret['query'] = req.query.q.toLowerCase();
+    ret['posts'] = [];
     
     var d = data['posts'];
-    var ids = data['aisle_post_ids'];
-    
-    var posts = [];
-    posts['query'] = req.query.q;
-    posts['posts'] = [];
+    var ids = Object.keys(d);
     
     for(var i=0; i < ids.length; i++) {
         if(d[ids[i]]['tags']) {
             for(var j=0; j< d[ids[i]]['tags'].length; j++){
                 if(d[ids[i]]['tags'][j]['tag'].toLowerCase() === req.query.q.toLowerCase()){
-                    posts['posts'].push(d[ids[i]]);
+                    ret['posts'].push(d[ids[i]]);
                 }
             }
         }
     }
     
-    
-    
-    res.render('search', posts);
+    res.render('search', ret);
 };
