3303510cebf8c568e4d655ee8aaeb5aa4e09424e
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-17 04:01:44 -0800

    Updated outfit.js to get logged in user properly and to get item posts from ids properly

--- routes/outfit.js
+++ routes/outfit.js
@@ -1,18 +1,19 @@
 var data = require('../data.json');
+var profile = require('./profile.js');
+var dashboard = require('./dashboard.js');
+
 exports.view = function(req, res) {
   var ret = {};
   // enclose in a list to do {{#each posts}}
   ret['posts'] = [data['posts'][req.query.id]];
-  ret['logged_in_user'] = data['logged_in_user'];
+  
+  ret['logged_in_user'] = profile.getloggedinuser(req);
   
   for(var i=0;i<ret['posts'].length;i++) {
     var post = ret['posts'][i];
     post['items'] = [];
     if(post['item_ids']) {
-      for(var j=0;j<post['item_ids'].length;j++) {
-        var item = data['posts'][post['item_ids'][j]];
-        post['items'].push(item);
-      }
+      post['items'] = dashboard.getpostsfromids(post['item_ids']);
     }
   }
   
