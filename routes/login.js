e7318f1de1cc77003c45676361a231bb9bb30c84
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-17 04:18:39 -0800

    Updated login.js to get logged in user properly and to log in and log out users properly

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var data = require('../data.json');
 var util = require('./util.js');
+var profile = require('./profile.js');
 
 exports.view = function(req, res) {
   res.render('login', {});
@@ -23,15 +24,18 @@ exports.loginuser = function(req, res) {
   // look for user -- TODO: change this later to generate user id
   var userid = util.getuserid(req.query.username);
   if(data['users'][userid]) {
-    setcurrentuser(userid);
+    console.log('login.js: logging in user ' + userid);
+    setcurrentuser(req, userid);
     res.redirect('/aisle');
   }
   console.log('login.js: login failed! User ' + userid + ' could not be found');
 };
 
 exports.logoutuser = function(req, res) {
-  if(data['logged_in_user'] && data['logged_in_user']['id'] == req.query.id) {
-    removecurrentuser();
+  var logged_in_user = profile.getloggedinuser(req);
+  if(logged_in_user && logged_in_user['id'] == req.query.id) {
+    console.log('login.js: logging out user ' + logged_in_user['id']);
+    removecurrentuser(req);
     res.redirect('/');
   }
 }
