390464692d43e4af71eb791f3c8f27a2fd44042c
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-22 19:09:39 -0800

    Updated outfit.js to work with mongo

--- routes/outfit.js
+++ routes/outfit.js
@@ -4,6 +4,37 @@ var models = require('../models');
 
 exports.view = function(req, res) {
   var ret = {};
+  
+  models.User
+    .find({'id': profile.getloggedinuser(req)})
+    .exec(afterUserSearch);
+    
+  function afterUserSearch(err, users) {
+    var loggedinuser = users[0];
+    // whether the user's logged in or not, they should be
+    // allowed to see outfits
+    dashboard.getpostsfromids(
+      [req.query.id], loggedinuser, function(err, posts) {
+        if(err){console.log(err); res.send(500);}
+        
+        var post = posts[0];
+        if(!post){
+          console.log('outfit.js: couldn\'t find any posts with id ' + req.query.id);
+          res.send(404);
+        }
+        
+        console.log('outfit.js: got outfit ' + post['id']);
+        ret['posts'] = [post];
+        if(loggedinuser) ret['logged_in_user'] = loggedinuser;
+        res.render('outfit', ret);
+      }
+    );
+  }
+  
+  
+  
+  
+  /*
   // enclose in a list to do {{#each posts}}
 
   console.log(req.query.id);
@@ -27,4 +58,5 @@ exports.view = function(req, res) {
                 res.end();
             }
         }
+        */
 };
