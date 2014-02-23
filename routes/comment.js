5f15d2383c4b7818e792893e1d09873e5bcb08b8
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-23 11:46:15 -0800

    Added mongo support to comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,40 @@
 var models = require('../models');
 
 exports.addcomment = function(req, res) {
-  var comment = {
-    'username': req.body.username,
-    'comment': req.body.comment,
-    'time': req.body.time,
-    'img': req.body.img
-  };
+  models.Post
+    .find({'id': req.body.postid})
+    .exec(function(err, result) {
+      if(err) {console.log(err);res.send(500);}
+      var post = result[0];
+      if(!post) {
+        console.log('comment.js: post ' + req.body.postid + ' not found');
+        res.send(404);
+      }
+      
+      var comment = new models.Comment({
+        'postid': req.body.postid,
+		'username': req.body.username,
+		'comment': req.body.comment,
+		'time': req.body.time,
+		'img': req.body.img
+      });
+      
+      comment.save(function(err) {
+        if(err) {console.log(err);res.send(500);}
+        if(!post['comment_ids']) post['comment_ids'] = [];
+        post['comment_ids'].unshift(comment['_id']);
+        
+        models.Post
+          .update({'id': post['id']}, {'comment_ids': post['comment_ids']},
+            function(err) {
+              if(err) {console.log(err); res.send(500);}
+              console.log('comment.js: saved comment successfully');
+              res.send(200);
+            })
+      });
+    });
   
+  /*
   models.Post
         .find({"id": req.body.postid})
         .exec(afterSearchPost);
@@ -27,5 +54,18 @@ exports.addcomment = function(req, res) {
                 res.writeHead(200);
                 res.end();
             }
-        }
+        }*/
 };
+
+exports.getcommentsfromids = function(ids, callback) {
+  if(!ids) {
+    if(callback) {
+      callback('comment.js: no ids', []);
+    }
+    return;
+  }
+  
+  models.Comment
+    .find({'_id': {$in : ids}})
+    .exec(callback);
+}
