00260caabb35ed6bc0b0a970e9dbffdd5abaf326
Jare Fagbemi <ofagbemi@stanford.edu>
2014-02-25 11:57:37 -0800

    Allowed users to add outfits to styles from the post page

--- routes/outfit.js
+++ routes/outfit.js
@@ -17,46 +17,31 @@ exports.view = function(req, res) {
       [req.query.id], loggedinuser, function(err, posts) {
         if(err){console.log(err); res.send(500);}
         
-        var post = posts[0];
-        if(!post){
-          console.log('outfit.js: couldn\'t find any posts with id ' + req.query.id);
-          res.send(404);
+        if(loggedinuser) {
+          dashboard.getpostsfromids(
+            loggedinuser['style_ids'],
+            loggedinuser,
+            function(err, styles) {
+              loggedinuser['styles'] = styles;
+              sendoutfit();
+            }
+          );
+        } else {
+          sendoutfit();
         }
-        
-        console.log('outfit.js: got outfit ' + post['id']);
-        ret['posts'] = [post];
-        if(loggedinuser) ret['logged_in_user'] = loggedinuser;
-        res.render('outfit', ret);
+        function sendoutfit() {
+		  var post = posts[0];
+		  if(!post){
+			console.log('outfit.js: couldn\'t find any posts with id ' + req.query.id);
+			res.send(404);
+		  }
+		
+		  console.log('outfit.js: got outfit ' + post['id']);
+		  ret['posts'] = [post];
+		  if(loggedinuser) ret['logged_in_user'] = loggedinuser;
+		  res.render('outfit', ret);
+        }
       }
     );
   }
-  
-  
-  
-  
-  /*
-  // enclose in a list to do {{#each posts}}
-
-  console.log(req.query.id);
-  models.Post.
-    find({"id": req.query.id}).exec(afterSearch);
-
-        function afterSearch(err, result) {
-            if(result[0]){
-               ret['posts'] = [result[0]];
-                ret['logged_in_user'] = profile.getloggedinuser(req);
-                for(var i=0;i<ret['posts'].length;i++) {
-                    var post = ret['posts'][i];
-                    post['items'] = [];
-                    if(post['item_ids']) {
-                        post['items'] = dashboard.getpostsfromids(post['item_ids']);
-                    }
-                }
-                res.render('outfit', ret);
-            } else {
-                res.writeHead(404);
-                res.end();
-            }
-        }
-        */
 };
